bef6b89d08f67e309873d9946ac32e8a26718bd3
notfooddev <66053378+notfooddev@users.noreply.github.com>
2020-06-04 01:01:07 +0200

    Add check if server settings exist for guild on ready (#5)
    * Add check if server settings exist for guild on ready
    
    * Removed nesting
    
    * Applied feedback

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,9 +1,30 @@
 import { Client as DiscordClient } from "discord.js";
 import Logger from "../lib/log";
+import ServerSettingsRepository from "../repository/severSettings";
 
 export default async function ReadyEvent(discordClient: DiscordClient) {
-	Logger.info(`Ready to serve`);
+	Logger.info(`Ready to serve, found ${discordClient.guilds.cache.size} guilds`);
 
-	// TODO:
-	// Check for any added guilds while offline to create settings objects for
+	await Promise.all(discordClient.guilds.cache.map(guild => CheckGuild(guild.id)))
+	async function CheckGuild(guildId: any) {
+		Logger.info(`Checking for guild ${guildId}`);
+	
+		const serverSettings = await ServerSettingsRepository.GetByGuildId(guildId)
+		if (serverSettings === null) {
+			ServerSettingsRepository.Save({
+				id: 0,
+				guildId: guildId,
+				deleted: null,
+				prefix: ';',
+				logChannel: null,
+				modLogChannel: null,
+				systemNotice: true,
+				streamLiveRole: null,
+				streamShout: null,
+				adminRole: null, 
+				moderatorRole: null,
+			});
+			Logger.info(`Created guild ${guildId}`);
+		}
+	}
 }
