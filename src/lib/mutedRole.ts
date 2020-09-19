90dc0c2637ef8e95b3f414429ea28781e4608c80
notfooddev <66053378+notfooddev@users.noreply.github.com>
2020-09-19 23:47:26 +0200

    Muted role should have permission to see muted channel (#38)
    * Muted role should get permission to see muted channel
    
    * Whitespace

--- src/lib/mutedRole.ts
+++ src/lib/mutedRole.ts
@@ -17,6 +17,7 @@ export async function SetMutedPermissions(mutedRole: Role) {
 export async function SetMutedPermissionsForChannel(mutedRole: Role, channel: GuildChannel, muteChannelId: string|null) {
 	if (!muteChannelId !== null && channel.id === muteChannelId) {
 		await channel.createOverwrite(mutedRole, {
+			VIEW_CHANNEL: true,
 			SEND_MESSAGES: true,
 			ADD_REACTIONS: false
 		}, "Automatic mute role permissions");
