4e49af42f759281603689c85fa6e64f4288dbfd0
Wes van der Vleuten <16665772+WesVleuten@users.noreply.github.com>
2020-11-09 18:21:07 +0100

    Added deleted by in message delete (#46)
    * Added deleted by in message delete
    
    * Added comments

--- src/events/messageDelete.ts
+++ src/events/messageDelete.ts
@@ -1,4 +1,4 @@
-import { Client as DiscordClient, Message } from "discord.js";
+import { Client as DiscordClient, Message, GuildAuditLogs } from "discord.js";
 import ActionLogRepository from "../repository/actionLog";
 import ServerSettingsRepository from "../repository/serverSettings";
 import Logger from "../lib/log";
@@ -6,8 +6,19 @@ import { ActionType } from "../interfaces/actionTypeEnum";
 import { getTextChannel } from "../lib/util";
 import createMessageEmbed from "../wrapper/discord/messageEmbed";
 
+const sleep = (ms: number) => new Promise(res => setTimeout(res, ms));
+
+/*
+ * MessageDeleteEvent
+ *
+ * It is not possible to exactly know who deleted a message, but by scanning
+ * the audit logs we can get a pretty clear idea of who was responsible.
+ * The "Deleted By" field is "Unkown" if we cannot figure out who deleted it
+ */
+
+
 export default async function MessageDeleteEvent(discordClient: DiscordClient, message: Message) {
-	if (message.author.bot) return;
+	if (!message.guild) return; // DM message
 
 	const serverSettings = await ServerSettingsRepository.GetByGuildId(message?.guild?.id);
 	if (serverSettings === null) {
@@ -18,6 +29,31 @@ export default async function MessageDeleteEvent(discordClient: DiscordClient, m
 	if (!serverSettings.logChannel) {
 		return;
 	}
+
+	await sleep(1000); // wait 1sec for audit logs to generate
+
+	const guild = message.guild;
+
+	// fetch last 7 audit logs
+	const auditLogs = await guild.fetchAuditLogs({
+		limit: 7,
+		type: 'MESSAGE_DELETE',
+	});
+
+	// search through audit logs and filter out any irrelevant logs
+	//  - match on target and author
+	//  - match on channel
+	//  - shouldn't be too long ago
+	const auditEntry = auditLogs.entries.find(x => {
+		const target: any = x.target;
+		const extra: any = x.extra;
+		return target.id === message.author.id &&
+			extra.channel.id === message.channel.id &&
+			Date.now() - x.createdTimestamp < 20000
+	});
+
+	// set the deleted by tag IF we found who deleted it
+	const deletedBy = auditEntry ? auditEntry.executor.tag : 'Unkown';
 
 	// add action to database
 	await ActionLogRepository.Add(serverSettings.id, message.author.id, ActionType.MessageDelete, message.channel.id, {
@@ -48,6 +84,10 @@ export default async function MessageDeleteEvent(discordClient: DiscordClient, m
 			{
 				key: "Message Deleted",
 				value: message.content,
+			},
+			{
+				key: "Deleted by",
+				value: deletedBy,
 			},
 		],
 	});
